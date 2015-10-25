2f520c45d91663f5fa35ac4d504d70f759ca795f
SwithFr <contact@swith.fr>
2015-10-25 16:52:05 +0100

    add some routes

--- bin/routes/users.js
+++ bin/routes/users.js
@@ -15,4 +15,19 @@ exports.init = function( oApp ) {
     // Login user
     oApp.post( "/user/check", require( "../controllers/users/check.js" ) );
 
+    // Get settings from user
+    oApp.get( "/user/settings/:id", require( "../controllers/users/settings.js" ) );
+
+    // Update settings from user
+    oApp.put( "/user/settings/:id", require( "../controllers/users/update-settings.js" ) );
+
+    // Update data from user
+    oApp.get( "/user/:id", require( "../controllers/users/details.js" ) );
+
+    // Get user's friends
+    oApp.get( "/user/friends", require( "../controllers/users/friends.js" ) );
+
+    // Add friend to user
+    oApp.post( "/user/friend", require( "../controllers/users/addFriend.js" ) );
+
 };
